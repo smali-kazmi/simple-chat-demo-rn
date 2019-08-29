8bd5675458c5a822d025339134848ef5c06241d5
Mudaser Ali <smali.kazmi@hotmail.com>
2019-08-30 02:51:46 +0500

    Task/chat app (#1)
    * add chat screen
    
    * add simple login screen
    
    * include chat screen
    
    * pass username to the next screen
    
    * display user name on screen
    
    * default user name
    
    * start basic chat
    
    * add message component
    
    * render message component
    
    * update gitignore
    
    * update gi
    
    * add socket server
    
    * enable socket communication
    
    * use localhost instead of url

---
+++ ChatApp/App/Containers/ChatScreen.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { ScrollView, Text, Image, View, TextInput, Button } from "react-native";
+import { Images } from "../Themes";
+import Message from "../Components/Message";
+import SocketIOClient from "socket.io-client";
+
+// Styles
+import styles from "./Styles/LaunchScreenStyles";
+
+export default class ChatScreen extends Component {
+  constructor(props) {
+    super(props);
+    const { state } = this.props.navigation;
+    const _this = this;
+    this.state = { name: state.params.name, text: "", messages: [] };
+    this.socket = SocketIOClient("http://localhost:3002", {
+      transports: ["polling", "websocket"]
+    });
+
+    this.receiveMessage = this.receiveMessage.bind(this);
+
+    this.socket.on("ChatApp:NewMsgRece", function(payload) {
+      console.log("ChatApp:NewMsgRece", payload);
+      _this.receiveMessage(payload);
+    });
+  }
+
+  sendMessage() {
+    console.log("i m called");
+    const sender = this.state.name;
+    const message = this.state.text;
+
+    const messages = this.state.messages;
+
+    this.socket.emit("ChatApp:NewMsg", { sender, message });
+    this.receiveMessage({ sender, message });
+  }
+
+  receiveMessage(message) {
+    const messages = this.state.messages;
+
+    messages.push(message);
+
+    this.setState({ text: "", messages: messages });
+
+    console.log(this.state.messages);
+  }
+  showMessages() {
+    return this.state.messages.length ? (
+      this.state.messages.map((message, index) => {
+        return <Message message={message} key={index} />;
+      })
+    ) : (
+      <View>
+        <Text>No New Message</Text>
+      </View>
+    );
+  }
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <ScrollView style={styles.container}>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>
+              {this.state.name}! Welcome to chat screen
+            </Text>
+          </View>
+          <View style={styles.section}>{this.showMessages()}</View>
+          <View style={styles.section}>
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: "gray",
+                borderWidth: 1,
+                marginTop: 10,
+                paddingLeft: 10
+              }}
+              onChangeText={text => this.setState({ text })}
+              value={this.state.text}
+            />
+            <Button title="Send" onPress={() => this.sendMessage()} />
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
--- ChatApp/App/Containers/LaunchScreen.js
+++ ChatApp/App/Containers/LaunchScreen.js
@@ -6,14 +6,37 @@ import { Images } from "../Themes";
 import styles from "./Styles/LaunchScreenStyles";
 
 export default class LaunchScreen extends Component {
+  static navigationOptions = {
+    title: "Welcome"
+  };
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = { text: "User" };
   }
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.mainContainer}>
-        <ScrollView style={styles.container}></ScrollView>
+        <ScrollView style={styles.container}>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Simple chat app</Text>
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: "gray",
+                borderWidth: 1,
+                marginTop: 10,
+                paddingLeft: 10
+              }}
+              onChangeText={text => this.setState({ text })}
+              value={this.state.text}
+            />
+            <Button
+              title="Enter"
+              onPress={() => navigate("ChatScreen", { name: this.state.text })}
+            />
+          </View>
+        </ScrollView>
       </View>
     );
   }
