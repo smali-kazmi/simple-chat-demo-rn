f67715532e61cc941dfe433f7b78ee0bb741e1d9
Mudaser Ali <smali.kazmi@hotmail.com>
2019-08-29 17:33:01 +0500

    add simple login screen

--- ChatApp/App/Containers/LaunchScreen.js
+++ ChatApp/App/Containers/LaunchScreen.js
@@ -13,7 +13,23 @@ export default class LaunchScreen extends Component {
   render() {
     return (
       <View style={styles.mainContainer}>
-        <ScrollView style={styles.container}></ScrollView>
+        <ScrollView style={styles.container}>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Simple chat app</Text>
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: "gray",
+                borderWidth: 1,
+                marginTop: 10,
+                paddingLeft: 10
+              }}
+              onChangeText={text => this.setState({ text })}
+              value={this.state.text}
+            />
+            <Button title="Enter" />
+          </View>
+        </ScrollView>
       </View>
     );
   }
